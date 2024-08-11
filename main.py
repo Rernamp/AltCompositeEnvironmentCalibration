1db1704dc96a45e21c9cc3f22f0d30930fa00abd
vlad230596 <vlad230596@mail.ru>
2024-08-11 22:02:43 +0300

    Refactor naming

--- main.py
+++ main.py
@@ -4,40 +4,41 @@ from scipy.spatial.transform import Rotation
 from scipy.spatial import distance
 from scipy.optimize import minimize
 
-
 from EniPy import eniUtils
 
-def get_mean_error(pointsA, pointsB, rotation_b, offset_b):
+
+def get_mean_error(points_a, points_b, rotation_b, offset_b):
     errors = []
-    for a, b in zip(pointsA, pointsB):
-        tB = rotation_b.apply(b) + offset_b
-        dist = distance.euclidean(a, tB)
+    for a, b in zip(points_a, points_b):
+        transformed = rotation_b.apply(b) + offset_b
+        dist = distance.euclidean(a, transformed)
         errors.append(dist)
     errors = np.array(errors)
     return errors.mean()
 
-def f(x, pointsA, pointsB):
-    r = Rotation.from_euler('xyz', angles=[0, x[0], 0], degrees=True)
-    return get_mean_error(pointsA, pointsB, r, x[1:])
 
-dumpPoints = eniUtils.readJson(f'dumps/5point.json')
+def f(x, points_a, points_b):
+    r = Rotation.from_euler('xyz', angles=[0, x[0], 0], degrees=True)
+    return get_mean_error(points_a, points_b, r, x[1:])
+
+
+dump_points = eniUtils.readJson(f'dumps/5point.json')
 
 pointsA = np.empty((0, 3))
 pointsB = np.empty((0, 3))
 
-for p in dumpPoints:
-    environmentPoses = p['EnvironmentPoses']
-    if len(environmentPoses) != 2:
+for p in dump_points:
+    environment_poses = p['EnvironmentPoses']
+    if len(environment_poses) != 2:
         raise Exception('Cuurently supported only 2 environment')
-    pointsA = np.append(pointsA, [environmentPoses[0]["Position"]], 0)
-    pointsB = np.append(pointsB, [environmentPoses[1]["Position"]], 0)
-
+    pointsA = np.append(pointsA, [environment_poses[0]["Position"]], 0)
+    pointsB = np.append(pointsB, [environment_poses[1]["Position"]], 0)
 
 base_rotation = 179.74
 base_position = np.array([-0.087, 0.036, -0.35])
 
 begin = time.perf_counter()
-result = minimize(f, [base_rotation, *base_position], args=(pointsA, pointsB))
+result = minimize(f, np.array([base_rotation, *base_position]), args=(pointsA, pointsB))
 end = time.perf_counter()
 print(f'completed by: {((end - begin) * 1000):1f} ms. Result: {result}')
 x = result["x"]
