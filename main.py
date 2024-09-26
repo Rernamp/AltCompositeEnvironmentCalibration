46acfe5def9cc2bb55c488bff72ad640673a93c9
vlad.mokhnachov <vlad230596@mail.ru>
2024-09-26 11:37:22 +0300

    Show errors for all points after test

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def f(x, points_a, points_b):
     return get_mean_error(points_a, points_b, r, x[1:])
 
 
-dump_points = eniUtils.readJson(f'dumps/5point.json')
+dump_points = eniUtils.readJson(f'dumps/OneAlt.ManualOffsetSamples.json')
 
 pointsA = np.empty((0, 3))
 pointsB = np.empty((0, 3))
@@ -30,12 +30,12 @@ pointsB = np.empty((0, 3))
 for p in dump_points:
     environment_poses = p['EnvironmentPoses']
     if len(environment_poses) != 2:
-        raise Exception('Cuurently supported only 2 environment')
+        raise Exception('Currently supported only 2 environment')
     pointsA = np.append(pointsA, [environment_poses[0]["Position"]], 0)
     pointsB = np.append(pointsB, [environment_poses[1]["Position"]], 0)
 
-base_rotation = 179.74
-base_position = np.array([-0.087, 0.036, -0.35])
+base_rotation = 0
+base_position = np.array([0, 0, 0])
 
 begin = time.perf_counter()
 result = minimize(f, np.array([base_rotation, *base_position]), args=(pointsA, pointsB))
@@ -43,3 +43,8 @@ end = time.perf_counter()
 print(f'completed by: {((end - begin) * 1000):1f} ms. Result: {result}')
 x = result["x"]
 print(f'error: {result["fun"] * 1000} mm. rotation: {x[0]} offset: {x[1:]}')
+
+r = Rotation.from_euler('xyz', angles=[0, x[0], 0], degrees=True)
+for a, b in zip(pointsA, pointsB):
+    e = get_mean_error([a], [b], r, x[1:])
+    print(f'{e * 1000:.2} mm')
