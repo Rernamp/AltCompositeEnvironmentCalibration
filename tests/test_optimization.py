1db400e13f4ff50d23e4475dc372a280957f869a
rernamp <renamp100@gmail.com>
2026-03-16 08:15:10 +0300

    add synthetic snapshots for test

--- tests/test_optimization.py
+++ tests/test_optimization.py
@@ -1,11 +1,45 @@
-from lmfit import Parameters, minimize, report_fit
+from lmfit import Parameter, Parameters, minimize, report_fit
 import numpy as np
+import copy
 import sys
+from random import randrange
 from pathlib import Path
 sys.path.insert(0, str(Path(__file__).parent.parent))
 
 from Utils import *
 
-guess, snapshots, markers = read_dump("dataset/allAxis_1Marker_fix3/#001.json")
+markers = np.array([[0,0,0], [0,1,0], [1,0,0], [1,1,0]])
 
-positions = np.array([snapshot.position for snapshot in snapshots])
+z_offset = 1
+
+positions = np.array([(marker + np.array([0,0, z_offset])) for marker in markers])
+
+print(f"Markers: {markers}")
+print(f"Positions: {positions}")
+
+synthetic_snapshots = []
+
+for i, position in enumerate(positions):
+    position = position
+    rays = []
+    marker_indices = []
+    for marker_index, marker in enumerate(markers):
+        ray = marker - position
+        marker_indices.append(marker_index)
+        rays.append(ray)
+    synthetic_snapshots.append(Snapshot(position=position, rays=rays, marker_indices=marker_indices))
+
+
+modified_snapshots = copy.deepcopy(synthetic_snapshots)
+
+
+
+max_offset = 0.1
+
+for i, snapshot in enumerate(modified_snapshots):
+    snapshot.position = snapshot.position + np.random.rand(snapshot.position.shape[0]) * max_offset
+    print(f"Modify position {i} {snapshot.position}")
+
+parameters = Parameters()
+
+for i, snapshot in modified_snapshots:
