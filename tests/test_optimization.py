e216a78fae01bd1cf602607c4b03c78459878934
rernamp <renamp100@gmail.com>
2026-03-17 08:22:59 +0300

    start add cost func

--- tests/test_optimization.py
+++ tests/test_optimization.py
@@ -1,4 +1,5 @@
-from lmfit import Parameter, Parameters, minimize, report_fit
+from Utils import *
+from lmfit import Parameter, Parameters, Minimizer, minimize, report_fit
 import numpy as np
 import copy
 import sys
@@ -6,13 +7,13 @@ from random import randrange
 from pathlib import Path
 sys.path.insert(0, str(Path(__file__).parent.parent))
 
-from Utils import *
 
-markers = np.array([[0,0,0], [0,1,0], [1,0,0], [1,1,0]])
+markers = np.array([[0, 0, 0], [0, 1, 0], [1, 0, 0], [1, 1, 0]])
 
 z_offset = 1
 
-positions = np.array([(marker + np.array([0,0, z_offset])) for marker in markers])
+positions = np.array([(marker + np.array([0, 0, z_offset]))
+                     for marker in markers])
 
 print(f"Markers: {markers}")
 print(f"Positions: {positions}")
@@ -27,19 +28,34 @@ for i, position in enumerate(positions):
         ray = marker - position
         marker_indices.append(marker_index)
         rays.append(ray)
-    synthetic_snapshots.append(Snapshot(position=position, rays=rays, marker_indices=marker_indices))
+    synthetic_snapshots.append(
+        Snapshot(position=position, rays=rays, marker_indices=marker_indices))
 
 
 modified_snapshots = copy.deepcopy(synthetic_snapshots)
 
 
-
 max_offset = 0.1
 
 for i, snapshot in enumerate(modified_snapshots):
-    snapshot.position = snapshot.position + np.random.rand(snapshot.position.shape[0]) * max_offset
+    snapshot.position = snapshot.position + \
+        np.random.rand(snapshot.position.shape[0]) * max_offset
     print(f"Modify position {i} {snapshot.position}")
 
 parameters = Parameters()
 
-for i, snapshot in modified_snapshots:
+for i, snapshot in enumerate(modified_snapshots):
+    add_point_to_parameters(parameters=parameters,
+                            point=snapshot.position, prefix=f"pos_{i}")
+
+print(parameters)
+
+
+def cost_func(parameters, snapshots, markers):
+    return 0
+    
+mini = Minimizer(cost_func, parameters, fcn_args=(modified_snapshots, markers))
+result = mini.minimize(method='BFGS')
+print(report_fit(result))
+    
+    
